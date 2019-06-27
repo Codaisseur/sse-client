5356a5a80b1b6c953cde750de9d0c13ef3d79c37
David Y. Stephenson <david@davidystephenson.com>
2019-06-27 10:12:53 +0200

    Receive events

---
+++ src/App.js
@@ -0,0 +1,21 @@
+/* global EventSource */
+
+import React from 'react'
+
+class App extends React.Component {
+
+  componentDidMount () {
+    const source = new EventSource('http://localhost:5000/stream')
+    source.onmessage = (event) => {
+      const { data } = event
+      const json = JSON.parse(data)
+      console.log('json test:', json)
+    }
+  }
+
+  render () {
+    return 'hello'
+  }
+}
+
+export default App
