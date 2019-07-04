73fd972413963a9e9811bdbb209983e249f8d097
David Y. Stephenson <david@davidystephenson.com>
2019-07-04 11:41:30 +0200

    Utilize state

--- src/App.js
+++ src/App.js
@@ -3,18 +3,26 @@
 import React from 'react'
 
 class App extends React.Component {
+  state = { messages: [] }
+
+  source = new EventSource('http://localhost:5000/stream')
 
   componentDidMount () {
-    const source = new EventSource('http://localhost:5000/stream')
-    source.onmessage = (event) => {
+    this.source.onmessage = (event) => {
       const { data } = event
-      const json = JSON.parse(data)
-      console.log('json test:', json)
+      const messages = JSON.parse(data)
+      console.log('messages test:', messages)
+      this.setState({ messages })
     }
   }
 
   render () {
-    return 'hello'
+    return this
+      .state
+      .messages
+      .map((message, index) => <p key={index}>
+        {message}
+      </p>)
   }
 }
 
