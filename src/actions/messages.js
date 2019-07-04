922ad726c8c848b64863f019e453ee58818bc901
David Y. Stephenson <david@davidystephenson.com>
2019-07-04 14:44:52 +0200

    Remove debug log

--- src/actions/messages.js
+++ src/actions/messages.js
@@ -3,7 +3,6 @@ export const EVENT = 'EVENT'
 export function onEvent (event) {
   const { data } = event
   const messages = JSON.parse(data)
-  console.log('messages test:', messages)
 
   return {
     type: EVENT,
